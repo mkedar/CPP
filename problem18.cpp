9bc986a77338ecb0e756ce5b4da8ecb91f270e01
mkedar <78320557+mkedar@users.noreply.github.com>
2023-02-10 13:32:40 -0800

    Add files via upload

---
+++ problem18.cpp
@@ -0,0 +1,42 @@
+/*
+CIST 004A Spring 2022
+HW week 2 problem 18
+Description: Uses inputeed interest rate, time compounded and principal to calculate total amount in savings and interest. 
+Input: principal, interestRate and compoundtimes. 
+Output: Interest rate, times  compounded, principal, interest, amount in savings 
+Student: Matan Kedar 
+Known bugs: none.
+Date: 2/10/2023
+*/
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main (){
+
+    // declare variables 
+    double principal, interestRate, compoundtimes;
+
+    // ask for input for variables
+    cout << "principal: " << endl;
+    cin >> principal;
+    cout << "interestRate: " << endl;
+    cin >> interestRate;
+    cout << "compoundtimes: " << endl;
+    cin >> compoundtimes;
+
+    // calculate values 
+    double amount = principal * pow((1+(interestRate/100)/compoundtimes),compoundtimes);
+    double interest = amount - principal;
+
+    // print all needed values 
+    cout << "Interest Rate:           " << interestRate << "%" << endl;
+    cout << "Times Compounded:        " << compoundtimes << endl;
+    cout << "Principal:              $" << principal << endl;
+    cout << "Interest:               $" << round(interest * 100.0)/ 100.0 << endl;
+    cout << "Amount in Savings:      $" << amount << endl;
+
+
+    return 0;
+}
