1b9784e0da8dd996fe483f4571d36b7cac9b3ef6
mkedar <78320557+mkedar@users.noreply.github.com>
2023-04-16 20:53:11 -0700

    Add files via upload

--- test2.cpp
+++ test2.cpp
@@ -1,15 +1,87 @@
-//Matan Kedar
-//3/2/2023
-
-#include <iomanip>
 #include <iostream>
 using namespace std;
 
-int main(){
-    float total = 0;
-    for(int i = 10; i <= 1249; i++){
-        total += (1.0) / ((i)*(i+1));
-    }
-    cout << fixed << setprecision(3) << total << endl;
+// Matan Kedar
+// 4/13/2023
+// CIST-004A Midterm 2 Part 2
 
+int line1(double enteries[], int size) {
+    int counter = 0; // checks which numbers are over 0 using simple for loops with nested if statement
+    for (int i = 0; i < size; i++) {
+        if (enteries[i] > 0) {
+            counter++;
+        }
+    }
+    return counter;
 }
+double line2(double enteries[], int size) {
+    double sum = 0; // finds the average of all the input postive numbers
+    for (int i = 0; i < size; i++) {
+        sum += enteries[i];
+    }
+    return sum / size;
+}
+double line3(double enteries[], int size) {
+    double min = enteries[0]; // find smallest value using for loop
+    for (int i = 1; i < size; i++) {
+        if (enteries[i] < min) {
+           min = enteries[i];
+        }
+    }
+    return min;
+}
+int line4(double enteries[], int size) {
+    double sum = 0; // simply calculates the average and adds to counter if num is bigger
+    for (int i = 0; i < size; i++) {
+        sum += enteries[i];
+    }
+    double average = sum / size;
+    int counter = 0;
+    for (int i = 0; i < size; i++) {
+        if (enteries[i] > average) {
+            counter++;
+        }
+    }
+    return counter;
+}
+void line5(double enteries[], int size) {
+    for (int i = 0; i < size - 1; i++) { // bubble sort
+        for (int j = 0; j < size - i - 1; j++) {
+            if (enteries[j] > enteries[j+1]) {
+                double temp = enteries[j];
+                enteries[j] = enteries[j+1];
+                enteries[j+1] = temp;
+            }
+        }
+    }
+    for(int z = 0; z < size; z++){
+        cout << enteries[z] << " ";
+    }
+}
+
+
+int main() {
+    double enteries[1001];
+    double input = 1;
+    int counter = 0;
+    while (input != 0) {
+        cin >> input;
+        enteries[counter] = input;
+        counter++;
+    }
+    cout << line1(enteries, counter - 1) << endl;
+    cout << line2(enteries, counter - 1) << endl;
+    cout << line3(enteries, counter - 1) << endl;
+    cout << line4(enteries, counter - 1) << endl;
+    line5(enteries, counter - 1);
+    
+    return 0;
+}
+
+/* output below for input values <3 4 5 1.1 6.9 0> :
+5
+4
+1.1
+2
+1.1 3 4 5 6.9
+*/
